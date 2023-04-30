dcd24f8ba3b9a6d99871f077c08a4a67945713f0
keenager <keenager84@gmail.com>
2023-04-30 23:21:18 +0900

    markdown tables 추가, simplemde 툴바 수정

--- blog/templatetags/blog_filter.py
+++ blog/templatetags/blog_filter.py
@@ -10,7 +10,7 @@ register = template.Library()
 def mark(value):
     toc = TocExtension(toc_class='toc card', permalink=True)
     extentions = ['nl2br', 'fenced_code',
-                  'markdown_del_ins', 'attr_list', toc]
+                  'markdown_del_ins', 'attr_list', toc, 'tables']
     md = Markdown(extensions=extentions)
     html = md.convert(value)
     return mark_safe(html)
